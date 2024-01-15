1cff7ea7dcdf8eac9295670268821ae9365e8ca6
YanKolev <y.kolev11@gmail.com>
2024-01-15 14:53:35 +0200

    initial functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const rock = rock
+const options = ["rock", "paper", "scrissors"]
+const choice = options[Math.floor(Math.random()*options.length)];
+
+function computerSelection(options){
+    return options;
+}
+
+function gamePlay(playerSelection, computerSelection){
+    if (playerSelection > computerSelection){
+        return "Player wins";
+    } else if (computerSelection > playerSelection){
+        return "Computer wins";
+    } else (computerSelection === playerSelection);{
+        return "It's a Tie";
+    }
+        
+}
+
+function singleRound(playerSelection,computerSelection){
+    
+}
