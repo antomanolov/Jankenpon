86f9f2e94c4370412a93ee1b68c5291c0c24a918
YanKolev <y.kolev11@gmail.com>
2024-01-15 15:29:57 +0200

    testing out functions

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const options = ["rock", "paper", "scrissors"]
-const choice = options[Math.floor(Math.random()*options.length)];
-
-function computerSelection(options){
-    return options;
-}
-
 function gamePlay(playerSelection, computerSelection){
     if (playerSelection > computerSelection){
         return "Player wins";
@@ -16,6 +9,31 @@ function gamePlay(playerSelection, computerSelection){
         
 }
 
-function singleRound(playerSelection,computerSelection){
-    
+/*Leet Code version
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber){
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
 }
+let computerSelection = getComputerChoice();
+console.log("Computer choice: ",computerSelection);*/
+
+function getComputerChoice(){
+    let choice = Math.random() * 3;
+if (choice <= 1) {
+    choice = "Rock";
+} else if (choice <= 2) {
+    choice = "Paper";
+} else {
+    choice = "Scissors";
+}
+return choice;// if your forget the return function it returns undefined as error
+}
+let computerSelection = getComputerChoice();
+console.log("Computer choice: ", computerSelection);
